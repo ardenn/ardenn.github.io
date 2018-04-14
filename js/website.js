3960f82fc90432cca354d9b85b351be0bf410bb2
ardenn <mc_alila@outlook.com>
2018-04-14 23:10:37 +0000

    Removed Ajax

--- js/website.js
+++ js/website.js
@@ -39,15 +39,3 @@ window.onscroll = () => {
     }
 }
 upScroll = () => keepScrollingUp(0, 1)
-
-$("#contact").submit(function (e) {
-    e.preventDefault();
-
-    var $form = $(this);
-    $.post(
-        $form.attr("action"),
-        $form.serialize()
-    ).then(function () {
-        alert("Thank you!");
-    });
-});
